8dcbc4023be7c83169168f36b93f03cabde84403
pobu168 <244202969@qq.com>
2021-11-26 09:34:01 +0800

    proxy config

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+const count = ref(0)
+
+onMounted(async () => {
+  // await getFissionCourseList().then((res: any) => {
+  //   console.log(res);
+  // });
+    axios({
+      method: 'post',
+      url: '/taskman/entities/request/query',
+      params: {
+      },
+    }).then(res => {
+      console.log(res);
+    });
+    axios({
+      method: 'get',
+      url: '/j/search_subjects?type=movie&tag=%E7%83%AD%E9%97%A8&page_limit=50&page_start=0',
+      params: {
+      },
+    }).then(res => {
+      console.log(res);
+    });
+  });
+</script>
+
+<template>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    proxy: {
+      // 字符串简写方式
+      '/taskman': 'http://106.52.160.142:19090/',
+      // 选项写法
+      '/j': {
+        target: 'https://movie.douban.com/', // 所要代理的目标地址
+        changeOrigin: true,  // true/false, Default: false - changes the origin of the host header to the target URL
+      }
+    }
+  }
+})
